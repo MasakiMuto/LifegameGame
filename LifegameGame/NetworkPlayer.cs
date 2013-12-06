7e6cee3bc80d1cd88084563d9da251225b0dce32
Masaki Muto <m.muto0128@gmail.com>
2013-12-06 17:02:54 +0900

    network test

--- LifegameGame/NetworkPlayer.cs
+++ LifegameGame/NetworkPlayer.cs
@@ -6,7 +6,37 @@ using System.Net.Sockets;
 
 namespace LifegameGame
 {
-	class NetworkPlayer : Player
+	public struct ConnectionInfo
 	{
+		public bool IsHost;
+		public System.Net.IPAddress TargetIP;
+		public int Port;
+	}
+
+	public class NetworkPlayer : Player
+	{
+		
+
+		Socket socket;
+
+		public NetworkPlayer(GameBoard board, GridState side, ConnectionInfo info)
+			: base(board, side)
+		{
+			if (info.IsHost)
+			{
+				var s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				socket = s.Accept();
+			}
+			else
+			{
+				socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				socket.Connect(info.TargetIP, info.Port);
+			}
+		}
+
+		public override bool Update()
+		{
+			throw new Exception();
+		}
 	}
 }
