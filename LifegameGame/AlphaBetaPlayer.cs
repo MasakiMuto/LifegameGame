099732e66815cc7990b96ca67272d2b83a203872
masaki muto <m.muto0128@gmail.com>
2013-12-13 12:24:21 +0900

    add debug log. Debug cutting logic

--- LifegameGame/AlphaBetaPlayer.cs
+++ LifegameGame/AlphaBetaPlayer.cs
@@ -22,9 +22,11 @@ namespace LifegameGame
 			var list = new PointScoreDictionary();
 
 			float alpha = float.MinValue;
+			var tree = TreeNode.Create(null, 0f);
+			tree = null;
 			foreach (var p in GetPlayablePoints().ToArray())
 			{
-				float score = EvalPosition(Board.PlayingBoard, p, ThinkDepth, this.Side, alpha);
+				float score = EvalPosition(Board.PlayingBoard, p, ThinkDepth, this.Side, alpha, tree);
 				list[p] = score;
 				if (score > alpha)
 				{
@@ -32,14 +34,18 @@ namespace LifegameGame
 				}
 				Board.SetBoardOrigin();
 			}
-
 			var res = GetMaxPoint(list);
+			if (tree != null)
+			{
+				TreeNode.SetValue(tree, res.Value);
+				System.IO.File.WriteAllText("tree.txt", tree.ToString());
+			}
 
 			return res.Key;
 		}
 
 
-		protected virtual float EvalPosition(BoardInstance board, Point p, int depth, CellState side, float alphaBeta)
+		protected virtual float EvalPosition(BoardInstance board, Point p, int depth, CellState side, float alphaBeta, TreeNode<float> t)
 		{
 			EvalCount++;
 			Board.PlayingBoard = board;
@@ -49,38 +55,43 @@ namespace LifegameGame
 			{
 				if (Board.GetWinner() == this.Side)
 				{
+					TreeNode.AddChild(t, GameBoard.WinnerBonus);
 					return GameBoard.WinnerBonus;
 				}
 				else
 				{
+					TreeNode.AddChild(t, -GameBoard.WinnerBonus);
 					return -GameBoard.WinnerBonus;
 				}
 			}
 			if (depth == 1)
 			{
-				return Board.EvalScore() * (int)(this.Side);
+				var s = Board.EvalScore() * (int)(this.Side);
+				TreeNode.AddChild(t, s);
+				return s;
 			}
 			else
 			{
 				bool isMax = this.Side != side;
 				float current = (isMax ? float.MinValue : float.MaxValue);
+				var tr = TreeNode.AddChild(t, 0);
 				foreach (var item in GetPlayablePoints().ToArray())
 				{
-					float score = EvalPosition(next, item, depth - 1, side == CellState.White ? CellState.Black : CellState.White, current);
-
+					float score = EvalPosition(next, item, depth - 1, side == CellState.White ? CellState.Black : CellState.White, current, tr);
 					if ((isMax && score > current) || (!isMax && score < current))
 					{
 						current = score;
 					}
-					if (!isMax && current > alphaBeta)
+					if (isMax && current >= alphaBeta)
 					{
 						break;
 					}
-					if (isMax && current < alphaBeta)
+					if (!isMax && current <= alphaBeta)
 					{
 						break;
 					}
 				}
+				TreeNode.SetValue(tr, current);
 				return current;
 			}
 		}
